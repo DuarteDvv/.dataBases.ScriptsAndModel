b3f44084072c48735ebfb3738fcd54efba7215f9
Luis Antonio Duarte Sousa <luisduarte.dev@gmail.com>
2024-02-16 20:47:30 -0300

    Update ScriptDDL_for_creation.sql

--- .mySQL.pharmacy/ScriptDDL_for_creation.sql
+++ .mySQL.pharmacy/ScriptDDL_for_creation.sql
@@ -7,10 +7,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema pharmacy
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema pharmacy
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `pharmacy` DEFAULT CHARACTER SET utf8mb3 ;
 USE `pharmacy` ;
 
